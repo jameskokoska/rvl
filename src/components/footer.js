b1c27fa0f16b97d7cea3fbb45bf289e7234d28c2
jameskokoska <jameskokoska@users.noreply.github.com>
2021-11-16 22:41:43 -0500

    changed navbar and footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,33 +7,8 @@ export default class Footer extends Component {
   render(){
     return (
       <div className="footer">
-        <div className="desktop-view">
-          <p className="accent-paragraph footer-copyright" style={{color:"gray"}}>{copyrightMessage}</p>
-        </div>
-        <div className="footer-socials">
-          {dataSocials.map((social, index)=>{
-            return <>
-              <FooterSocial key={social.name} social={social}/>
-            </>
-          })}
-        </div>
-        <div className="mobile-view">
-          <p className="accent-paragraph footer-copyright" style={{color:"gray"}}>{copyrightMessage}</p>
-        </div>
+        <p className="accent-paragraph footer-copyright" style={{color:"gray"}}>{copyrightMessage}</p>
       </div>
     );
-  }
-}
-
-export class FooterSocial extends Component {
-  render(){
-    return(
-      <a href={this.props.social.link} style={{textDecorationColor:"transparent"}}>
-        <div className="footer-social">
-          <img alt={this.props.social.name} className="footer-social-image" src={process.env.PUBLIC_URL+"/"+this.props.social.icon}/>
-          {/* <p className="accent-paragraph footer-social-label" style={{color:"black"}}>{this.props.social.name}</p> */}
-        </div>
-      </a>
-    )
   }
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,8 +2,7 @@ import React,{Component} from 'react'
 import './navbar.css';
 import {pages} from "../util/pages"
 import {Link} from "react-router-dom";
-// import { FooterSocial } from './footer';
-// import {dataSocials} from "../data/socials.js"
+import { dataSocials } from '../data/socials';
 
 export class Navbar extends Component {
   constructor() {
@@ -33,20 +32,33 @@ export class Navbar extends Component {
     return(
       <div className="navbar">
         <div className="desktop-view">
-          <div className="navbar-flex horizontal-padding">
-          <Link to="/"><img alt="RVL" style={{height:"50px", padding:"5px", marginRight:"20px"}} src={require("../assets/RVL-icon.png").default}/></Link>
-            <div>
-              {
-                this.navbarPagesTotal.map((item,index)=>{
-                  return <NavbarLink selected={this.state.currentLink===item.link} title={item.title} link={item.link}/>
-                })
-              }
+          <div className="navbar-flex">
+            <Link to="/"><img alt="RVL" style={{height:"40px", padding:"5px", marginRight:"20px", position:"absolute", left:"7px", top:"4px"}} src={require("../assets/RVL-icon.png").default}/></Link>
+            {
+              this.navbarPagesTotal.map((item,index)=>{
+                return <NavbarLink selected={this.state.currentLink===item.link} title={item.title} link={item.link}/>
+              })
+            }
+            <div className="navbar-socials" style={{position:"absolute", right:"5px", top:"7px"}}>
+              {dataSocials.map((social, index)=>{
+                return <>
+                  <NavbarSocial key={social.name} social={social}/>
+                </>
+              })}
             </div>
           </div>
         </div>
         <div className="mobile-view">
           <div className="navbar-flex" style={{zIndex:100, backgroundColor:"white"}}>
-            <Link to="/"><img alt="RVL" style={{height:"40px", padding:"5px", marginLeft:"5px"}} src={require("../assets/RVL-icon.png").default}/></Link>
+            <Link to="/"><img alt="RVL" style={{height:"40px", padding:"5px", marginRight:"20px", position:"absolute", left:"5px", top:"4px"}} src={require("../assets/RVL-icon.png").default}/></Link>
+            <div style={{height:"50px"}}/>
+            <div className="navbar-socials" style={{position:"absolute", right:"40px", top:"11px"}}>
+              {dataSocials.map((social, index)=>{
+                return <>
+                  <NavbarSocial key={social.name} social={social}/>
+                </>
+              })}
+            </div>
             <img onClick={()=>{this.setState({open:!this.state.open})}} alt="menu" className="navbar-menu-icon" src={require("../assets/buttons/bars-solid.svg").default}/>
           </div>
           <div className={"navbar-items-mobile " + (!this.state.open?"navbar-items-mobile-open":"")}>
@@ -87,6 +99,16 @@ class NavbarLinkMobile extends Component {
   }
 }
 
+
+export class NavbarSocial extends Component {
+  render(){
+    return(
+      <a className="navbar-social" href={this.props.social.link} style={{textDecorationColor:"transparent"}}>
+        <img alt={this.props.social.name} className="navbar-social-image" src={process.env.PUBLIC_URL+"/"+this.props.social.icon}/>
+      </a>
+    )
+  }
+}
 
 export class NavbarSpace extends Component {
   render(){
